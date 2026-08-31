8be7eba2aa120831896f98b6f05333033c1b77b9
aashizzCodes <aashish26052007@gmail.com>
2026-08-31 20:53:33 +0530

    initial commit

---
+++ backend/check_location.py
@@ -0,0 +1,16 @@
+# this file manages the location 
+
+
+
+from typing import Any
+
+
+class CheckLocation:
+
+    def __init__(self) -> None:
+        
+        self.allocated_location_list = {}
+
+
+    def is_vechile_in_no_parking(self , location ) -> Any:
+        
