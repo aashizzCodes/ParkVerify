a35959a9a3d1eb331289248cbf0e29fb05d814ec
aashizzCodes <aashish26052007@gmail.com>
2026-09-09 19:14:57 +0530

    fixed some logic

--- backend/identify_vehicle.py
+++ backend/identify_vehicle.py
@@ -10,6 +10,7 @@ Docstring for backend.identify_vehicle
 
 from ultralytics import YOLO
 from typing import Any
+import cv2
 
 
 # ------------------------------------------------------------------------------
@@ -22,13 +23,13 @@ class VehicleInfo:
         self.license_plate_identification_model = YOLO(r'model\license-plate-finetune-v1n.pt')
 
 
-    # identify the vechile by type 
-    def identify_vechile_type( self , image_path ) -> Any:
+    # get vehicle data
+    def identify_vechile_data( self , image_path ) -> list:
 
         # this returns results of the prediction 
         results = self.vechile_identification_model(image_path)
 
-        # empty array for identified vechiles
+        # empty list for identified vechiles
         vehicle_data = []
 
         # loop through the results
@@ -41,16 +42,42 @@ class VehicleInfo:
                 vehicle_data.append({
                     'vehcile_type' : vehcile_type ,
                     'confidence' : float(box.conf[0]) , 
-                    'coordinates' : box.xyxy[0].int().tolist()
+                    'coordinates' : box.xyxy[0].int().tolist() ,
+                    'image_path' : image_path
                 })
                 
-        # return the detials to the function 
+        # return the data to the function 
         return vehicle_data
 
 # ------------------------------------------------------------------------------
 
     # fucntion for getting the vechile number if exits
-    def get_vechile_license_plate_number(self,image_path):
-        
-        
+    def get_vechile_license_plate_number(self , image_path ) -> Any:
+
+        # get the coords using the identify_vechile_type function
+        vehicle_data = self.identify_vechile_data( image_path )
+
+        # process the image using cv2 
+        processed_image = cv2.imread(image_path)
+
+        # loop into the items 
+        for vehicle in vehicle_data:
+
+            # get the coords of the vechile detected
+            vehile_x1 , vehile_y1 , vehile_x2 , vehile_y2 = vehicle['coordinates']
+
+            # crop the image using cv2 [vehicle]
+            vehile_cropped_image = processed_image[ vehile_y1:vehile_y2 , vehile_x1:vehile_x2 ]
+
+            # check if the cropped image has license plate visible in it 
+
+            if()
+
+
 # ------------------------------------------------------------------------------
+
+
+
+c  = VehicleInfo()
+
+print(c.identify_vechile_data(r'img\number_plated_car.jpg'))
